265c30af9d04fe61509e90d6c3cedf1b34f27bae
Petar Dobrev <petar.dobrev92@gmail.com>
2022-11-28 22:27:03 +0200

    feat: add data attribute

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
-
+  const product = document.querySelector(".product");
+  const priceValue = document.querySelector(".price").innerText;
+  product.setAttribute("data-price", priceValue);
 });
